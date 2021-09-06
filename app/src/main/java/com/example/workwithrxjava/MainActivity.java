61115b1c6deab58fcefba213d1cd5f2d71b7739a
amir.e <amir.h.esmailpor@gmail.com>
2021-09-06 21:37:24 +0430

    add DisposableObserver and CompositeDisposable to the project

--- app/src/main/java/com/example/workwithrxjava/MainActivity.java
+++ app/src/main/java/com/example/workwithrxjava/MainActivity.java
@@ -1,48 +1,62 @@
 package com.example.workwithrxjava;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.reactivex.Observable;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private Disposable disposable;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> animalObservable = Observable.just("Eagle", "Bee", "Lion", "Dog", "Wolf");
-        Observer<String> animalObserver = getAnimalObserver();
+        Observable<String> animalObservable = Observable.fromArray(
+                "Ant", "Ape",
+                "Bat", "Bee", "Bear", "Butterfly",
+                "Cat", "Crab", "Cod",
+                "Dog", "Dove",
+                "Fox", "Frog");
+
+        DisposableObserver<String> animalDisposableObserver = getAnimalObserver();
+        DisposableObserver<String> animalsObserverAllCaps = getAnimalsObserverAllCaps();
 
         // make Observer a member of the Observable till it can receive the data
-        animalObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(animalObserver);
+        compositeDisposable.add(
+                animalObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(s -> s.toLowerCase().startsWith("b"))
+                        .subscribeWith(animalDisposableObserver));
+
+        compositeDisposable.add(
+                animalObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                .filter(s -> s.toLowerCase().startsWith("c"))
+                .map(s -> s.toUpperCase())
+                .subscribeWith(animalsObserverAllCaps)
+        );
     }
 
-    // observe the data which is emitted from Observable
-    private Observer<String> getAnimalObserver(){
-        return new Observer<String>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.d(TAG, "onSubscribe");
-            }
 
+    // observe the data which is emitted from DisposableObservable
+    private DisposableObserver<String> getAnimalObserver() {
+        return new DisposableObserver<String>() {
             @Override
             public void onNext(@NonNull String s) {
                 Log.d(TAG, "Name: " + s);
@@ -60,12 +74,29 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    private DisposableObserver<String> getAnimalsObserverAllCaps() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(@io.reactivex.annotations.NonNull String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
     @Override
     protected void onDestroy() {
-
         // don't send events once the activity is destroyed
-        disposable.dispose();
-
+        compositeDisposable.clear();
         super.onDestroy();
     }
 }
